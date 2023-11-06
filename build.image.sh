566b159db8a99a6a06212a6d7af01d07a0b48e09
lixianmin <lixianmin@live.cn>
2023-11-07 02:18:34 +0800

    输出linux/amd64格式的image

--- build.image.sh
+++ build.image.sh
@@ -1,2 +1,6 @@
-docker build -t lixianmin/golang-mini:latest .
+docker build --platform linux/amd64 -t lixianmin/golang-mini:latest .
+
+# if returns: denied: requested access to the resource is denied
+# you need to run: docker login 
 docker push lixianmin/golang-mini:latest
+
